05e47aefbe30203b7c770a96700a2c6154c740b9
feuvpi <fredvpg@gmail.com>
2022-07-02 20:29:40 -0300

    note model created.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Note
 
 # Register your models here.
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('note', 'note_date')
+    list_filter = ('note_date',)
+
+admin.site.register(Note, NoteAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Note(models.Model):
+    note = models.TextField()
+    note_date = models.DateTimeField(auto_now=True, verbose_name='Note Date')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'note'
+
+    def __str_(self):
+        return self.note
+
+
