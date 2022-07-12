d917ebcadc23f3490a7a09bcbb72ac3706d07c1f
feuvpi <fredvpg@gmail.com>
2022-07-12 18:19:52 -0300

    displaying id on admin panel.

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,7 @@ from core.models import Note
 # Register your models here.
 
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('note', 'note_date')
+    list_display = ('id', 'note', 'note_date')
     list_filter = ('note_date',)
 
 admin.site.register(Note, NoteAdmin)
